328e112a0ad829ce43c2715ebece143792b670e0
Fabio Riva <f.riva@sotefin.ch>
2024-08-28 15:11:05 +0200

    Add files via upload

---
+++ Plc.js
@@ -0,0 +1,101 @@
+const EventEmitter = require('events')
+const logger = require('pino')()
+const snap7 = require('node-snap7')
+const util = require('util')
+
+class PLC extends EventEmitter {
+  constructor (ip, rack, slot, time) {
+    super()
+    this.client = new snap7.S7Client()
+    this.online = false
+    this.ip = ip
+    this.rack = Number(rack)
+    this.slot = Number(slot)
+    this.time = Number(time)
+  }
+
+  error (e) {
+    this.online = !this.client.Disconnect()
+    isNaN(e) ? logger.error(e) : logger.error(this.client.ErrorText(e))
+  }
+
+  async read (area, dbNumber, start, amount, wordLen) {
+    try {
+      const buffer = await ReadArea(this.client, area, dbNumber, start, amount, wordLen)
+      return buffer
+    } catch (e) {
+      this.error(e)
+    }
+  }
+
+  async write (area, dbNumber, start, amount, wordLen, buffer) {
+    try {
+      const res = await WriteArea(this.client, area, dbNumber, start, amount, wordLen, buffer)
+      return res
+    } catch (e) {
+      this.error(e)
+    }
+  }
+
+  forever (timeout) {
+    setTimeout(async () => {
+      try {
+        if (this.online) {
+          const buffer = await this.read(0x84, 37, 0, 4, 0x02)
+          this.page = buffer.readInt16BE(0)
+          this.card = buffer.readInt16BE(2)
+          // console.log(buffer, this)
+        } else {
+          this.online = this.client.Connect()
+          this.online ? logger.info('Connected to PLC %s', this.ip) : logger.info('Connecting to PLC %s ...', this.ip)
+        }
+        this.publish('api/kiosk/info', {
+          card: this.card,
+          comm: this.online,
+          page: this.page
+        })
+      } catch (e) {
+        this.error(e)
+      }
+      this.forever(timeout)
+    }, timeout)
+  }
+
+  publish (channel, data) {
+    this.emit('pub', { channel, data: Buffer.from(JSON.stringify(data)) })
+  }
+
+  run () {
+    try {
+      this.online = this.client.ConnectTo(this.ip, this.rack, this.slot)
+      this.forever(this.time)
+    } catch (e) {
+      this.error(e)
+    }
+  }
+}
+
+const ReadArea = util.promisify(
+  (client, area, dbNumber, start, amount, wordLen, callback) => {
+    client.ReadArea(area, dbNumber, start, amount, wordLen, function (
+      err,
+      data
+    ) {
+      if (err) return callback(err)
+      callback(err, data)
+    })
+  }
+)
+
+const WriteArea = util.promisify(
+  (client, area, dbNumber, start, amount, wordLen, buffer, callback) => {
+    client.WriteArea(area, dbNumber, start, amount, wordLen, buffer, function (
+      err
+    ) {
+      if (err) return callback(err)
+      callback(err, true)
+    })
+  }
+)
+
+module.exports = PLC
---
+++ index.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+const uWS = require('uWebSockets.js')
+const logger = require('pino')()
+const Plc = require('./Plc')
+const { readJson, sendJson } = require('./json')
+
+const log = (req) => {
+  logger.info({
+    'user-agent': req.getHeader('user-agent'),
+    method: req.getMethod(),
+    url: req.getUrl()
+  })
+}
+
+const CLS = {
+  area: 0x84,
+  dbNr: 37,
+  start: 4,
+  amount: 2,
+  wordLen: 0x02
+}
+const PIN = {
+  area: 0x84,
+  dbNr: 37,
+  start: 6,
+  amount: 2,
+  wordLen: 0x02
+}
+const PATH = '/api/kiosk'
+
+const main = async () => {
+  try {
+    const app = uWS.App().listen(Number(process.env.PORT), token => logger.info(token))
+    app.get('/*', (res, req) => {
+      log(req)
+      res.end('Resource not found')
+    })
+    // app.get(PATH + '/read', async (res, req) => {
+    //   log(req)
+    //   res.onAborted(() => {
+    //     res.aborted = true
+    //   })
+    //   const buffer = await plc.read(0x84, 510, 0, 20, 0x02)
+    //   sendJson(res, { hello: buffer })
+    // })
+    app.get(PATH + '/press', async (res, req) => {
+      log(req)
+      res.onAborted(() => {
+        res.aborted = true
+      })
+      const buffer = Buffer.allocUnsafe(2)
+      buffer.writeUInt16BE(1, 0)
+      const { area, dbNr, start, amount, wordLen } = CLS
+      const done = await plc.write(area, dbNr, start, amount, wordLen, buffer)
+      sendJson(res, { message: done ? 'closing' : 'error' })
+    })
+    app.get(PATH + '/unpress', async (res, req) => {
+      log(req)
+      res.onAborted(() => {
+        res.aborted = true
+      })
+      const buffer = Buffer.allocUnsafe(2)
+      buffer.writeUInt16BE(0, 0)
+      const { area, dbNr, start, amount, wordLen } = CLS
+      const done = await plc.write(area, dbNr, start, amount, wordLen, buffer)
+      sendJson(res, { message: done ? 'opening' : 'error' })
+    })
+    app.post(PATH + '/pin', async (res, req) => {
+      log(req)
+      readJson(
+        res,
+        async json => {
+          const regexp = /^[a-fA-F0-9]{3}$/
+          // console.log(json, regexp.test(json.pin))
+          const buffer = Buffer.alloc(2)
+          buffer.writeInt16BE(parseInt(json.pin, 16), 0) // string to hex
+          const { area, dbNr, start, amount, wordLen } = PIN
+          const done = await plc.write(area, dbNr, start, amount, wordLen, buffer)
+          // console.log(buffer, done)
+          sendJson(res, json)
+        })
+    })
+    app.ws(PATH + '/info', { open: ws => ws.subscribe('api/kiosk/info') })
+    // PLC
+    const plc = new Plc(process.env.PLC_IP, process.env.PLC_RACK, process.env.PLC_SLOT, process.env.PLC_TIME)
+    plc.run()
+    plc.on('pub', ({ channel, data }) => app.publish(channel, data))
+  } catch (err) {
+    logger.error(new Error(err))
+    process.exit(1)
+  }
+}
+
+main()
